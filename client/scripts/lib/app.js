d437194e6f9e75f896fbfa5f2426aa982a805aa1
Vaccuss <Deansteven308@gmail.com>
2016-03-01 18:47:26 +1000

    Setting up Food_Rescue client server in meteor using iconic

---
+++ client/scripts/lib/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Dean on 3/1/2016.
+ */
+angular.module('Whatsapp', [
+        'angular-meteor',
+        'ionic'
+    ]);
+
+if (Meteor.isCordova) {
+    angular.element(document).on('deviceready', onReady);
+}
+
+else {
+    angular.element(document).ready(onReady);
+}
+
+function onReady() {
+    angular.bootstrap(document, ['Whatsapp']);
+}
---
+++ client/scripts/routes.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Dean on 3/1/2016.
+ */
+angular
+    .module('Whatsapp')
+    .config(config);
+
+function config($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('tab', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'client/templates/tabs.html'
+        })
+        .state('tab.chats', {
+            url: '/chats',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'client/templates/chats.html'
+                }
+            }
+        });
+
+    $urlRouterProvider.otherwise('tab/chats');
+}
