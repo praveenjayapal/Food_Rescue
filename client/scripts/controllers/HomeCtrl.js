9873acf40c1996b303819db82804fc225eb5529b
Vaccuss <Deansteven308@gmail.com>
2016-03-04 14:31:15 +1000

    Update -- working

---
+++ client/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Dean on 3/1/2016.
+ */
+angular
+    .module('Whatsapp')
+    .controller('HomeCtrl', HomeCtrl);
+
+function HomeCtrl($scope, $reactive) {
+    $reactive(this).attach($scope);
+   this.home_data = "Welcome"
+}
--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -1,26 +1,80 @@
 /**
  * Created by Dean on 3/1/2016.
  */
-angular
-    .module('Whatsapp')
-    .config(config);
 
-function config($stateProvider, $urlRouterProvider) {
+if (Meteor.isCordova) {
+    angular
+        .module('Whatsapp')
+        .config(mobile_config);
+}
+else {
+    angular
+        .module('Whatsapp')
+        .config(web_config);
+}
+
+
+function mobile_config($stateProvider, $urlRouterProvider) {
+    // Conditional to check if the app should be a mobile app.
+    // Or a web browser app.
     $stateProvider
         .state('tab', {
             url: '/tab',
             abstract: true,
-            templateUrl: 'client/templates/tabs.html'
+            templateUrl: 'client/templates/mobile/tabs.html'
         })
+
         .state('tab.chats', {
             url: '/chats',
             views: {
                 'tab-chats': {
-                    templateUrl: 'client/templates/chats.html',
+                    templateUrl: 'client/templates/mobile/chats.html',
                     controller: 'ChatsCtrl as chats'
+                }
+            }
+        })
+
+        .state('tab.home', {
+            url: '/home',
+            views: {
+                'tab-home': {
+                    templateUrl: 'client/templates/mobile/home.html',
+                    controller: 'HomeCtrl as home'
                 }
             }
         });
 
-    $urlRouterProvider.otherwise('tab/chats');
+    $urlRouterProvider.otherwise('tab/home');
+}
+
+function web_config($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('tab', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'client/templates/mobile/tabs.html'
+        })
+
+        .state('tab.chats', {
+            url: '/chats',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'client/templates/mobile/chats.html',
+                    controller: 'ChatsCtrl as chats'
+                }
+            }
+        })
+
+        .state('tab.home', {
+            url: '/home',
+            views: {
+                'tab-home': {
+                    templateUrl: 'client/templates/mobile/home.html',
+                    controller: 'HomeCtrl as home'
+                }
+            }
+        });
+
+    $urlRouterProvider.otherwise('tab/home');
+
 }
